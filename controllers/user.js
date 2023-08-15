8d6229944da8916c49628ddc6d4281b790a56153
Aayu-shh <aayush28.official@gmail.com>
2023-08-16 01:18:32 +0530

    T6:ENCRYPTED User pass Fixed Login Auth acordingly

--- controllers/user.js
+++ controllers/user.js
@@ -1,42 +1,52 @@
 const User = require('../models/user');
-exports.signup = (req, res, next) => {
+const bcrypt = require('bcrypt');
+
+exports.signup = async (req, res) => {
     const name = req.body.named;
     const email = req.body.emailed;
     const password = req.body.passed;
     // console.log(name+" n "+password+" p "+email+" e ")
-    User.create({ Name: name, Email: email, Password: password })
-        .then(result => {
-            console.log(result.dataValues);
-            res.send(result)
-        })
-        .catch(err => res.send(err));
+    try {
+        const hash = await bcrypt.hash(password, 10);
+        const result = User.create({ Name: name, Email: email, Password: hash });
+        console.log(result.dataValues);
+        res.send(result);
+    }
+    catch (err) {
+        res.send(err);
+    }
 }
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res) => {
     const email = req.body.emailed;
     const password = req.body.passed;
-    // console.log(name+" n "+password+" p "+email+" e ")
-    User.findAll({where:{
-        Email: email,
-        Password: password
-    }})
-    .then(result =>{
-        //console.log(result);
-        if (result.length)      //if len>0  => Correct Credentials FOUND
-            res.send({
-                message:'User logged in Succesfully',
-                success: true
-        });
-        else
-            User.findAll({where:{
-                Email:email
-            }})
-            .then(elseResult =>{
-                if(elseResult.length)       //if len>0 => Email FOUND but Password didn't match (from outer IF)
-                    res.status(401).send('User NOT Authorized !')
-                else
-                    res.status(404).send('User NOT Found !')
-            })
-    })
-    .catch(err=>console.log(err));
+    try {
+        const result = await User.findAll({
+            where: {
+                Email: email,
+            }
+        })
+        // console.log(result);
+        if (result.length)      //if len>0  =>  Email FOUND
+        {
+            const dbHash = result[0].dataValues.Password;
+            console.log(dbHash + " ==> is the Password fetched From Database.");
+            const isAuthorized = await bcrypt.compare(password, dbHash);
+            if (isAuthorized) {
+                res.send({
+                    message: 'User logged in Succesfully',
+                    success: true
+                });
+            }
+            else {
+                res.status(401).send('Wrong password entered, User NOT Authorized !');
+            }
+        }
+        else {
+            res.status(404).send('User NOT Found in Database!')
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
