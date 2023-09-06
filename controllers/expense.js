2649cdfdbc40c124a4469e22f51f8ea7408152d7
Aayu-shh <aayush28.official@gmail.com>
2023-09-06 18:04:17 +0530

    T13: Making app Production ready with Transactions

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,8 +1,10 @@
 const Expense = require('../models/expense');
 const User = require('../models/user');
+const sequelize = require('../util/database');
 
 
 exports.addExpense = async (req, res) => {
+    const t = await sequelize.transaction();
     const { Amount, Description, Category } = req.body;        //Extracting properties 
     try {
         const expenseObj = {
@@ -11,15 +13,17 @@ exports.addExpense = async (req, res) => {
             Category: Category,
         };
         //using Sequelize Magic Function createExpense()
-        const expense = await req.user.createExpense(expenseObj);
+        const expense = await req.user.createExpense(expenseObj, { transaction: t });
         const totalExp1 = req.user.totalExpense;
         const newTotalExp = parseInt(totalExp1) + parseInt(Amount);
-        const respTotal = await req.user.update({'totalExpense': newTotalExp})
-        
+        await req.user.update({ 'totalExpense': newTotalExp }, { transaction: t })
+        await t.commit();
         return res.send(expense);
     }
     catch (err) {
+        await t.rollback();
         console.log(err);
+        res.status(500).json({ success: false, error: err });
     }
 }
 
@@ -34,19 +38,23 @@ exports.getExpenses = async (req, res) => {
 }
 
 exports.deleteExpense = async (req, res) => {
+    const t = await sequelize.transaction();
+    const newExp = parseInt(req.user.totalExpense) - parseInt(req.body.Amount);
     try {
-        console.log(req.user);
-        const newExp = parseInt(req.user.totalExpense) - parseInt(req.body.Amount);
-        const updatedTotal = await req.user.update({'totalExpense': newExp});
-        const response = await Expense.destroy({ where: { id: req.body.id } });
+        const updatedTotal = await req.user.update({ 'totalExpense': newExp }, { transaction: t });
+        const response = await Expense.destroy({ where: { id: req.body.id } }, { transaction: t });
         if (response == 1) {
+            await t.commit();
             res.send(`Succesfully deleted expense with ID: ${req.body.id}`)
         }
         else {
+            await t.rollback();
             res.send('Something went Wrong! Check the Code!');
         }
     }
     catch (err) {
+        await t.rollback();
         console.log(err);
+        res.status(500).json({success:false,error:err});
     }
 }
