3c1c41613fb1232a5e20b557c76aac046eab2ec5
Aayu-shh <aayush28.official@gmail.com>
2023-12-23 16:07:43 +0530

    Reorganizing to Frontend Backend

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'views')));
+//app.use(express.static(path.join(__dirname, 'views')));
 //app.set('views','views');
 
 app.get('/', (req, res, next) => {
